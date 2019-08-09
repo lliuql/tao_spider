1492759678d68d7da9adfafe9c57909e54971d1f
liuqh <550889982@qq.com>
2019-08-09 15:03:15 +0800

    第一次提交

---
+++ src/creds.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yuan on 2018/9/12.
+ */
+
+module.exports = {
+    username:'',
+    password:''
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+const CREDS = require('./creds');
+
+async function taobaoLogin() {
+  // const browser = await puppeteer.launch();
+  // const page = await browser.newPage();
+  // await page.goto('https://example.com');
+  // await page.screenshot({path: 'example.png'});
+ 
+  // await browser.close();
+  
+} 
+
+taobaoLogin()
+
